0e56f9c7072b2283d61db4225ce725f9a967ddff
Joe-metry <joemetry150@gmail.com>
2023-06-28 05:59:20 -0700

    Practice on Puppet and it use

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Automating file configuration process using Puppet
 
-file { 'etc/ssh/ssh_config':
+ file { 'etc/ssh/ssh_config':
 	ensure => present,
 
-content =>"
+ content =>"
 
 	#SSH client configuration
 	host*
